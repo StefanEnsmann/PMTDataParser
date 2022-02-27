71fc7329d1e472d84b049153201a71df309be30b
Stefan Ensmann <stefan@ensmann.de>
2022-02-27 22:28:54 +0100

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+function exportRuleset(ruleset) {
+
+}
+
+function importRulesetForEdit(path) {
+
+}
+
+function importRulesetForPlay(path) {
+
+}
+
+function validateRuleset(path) {
+
+}
+
+function generateDetailsForRuleset(path) {
+
+}
+
+export { exportRuleset, importRulesetForEdit, importRulesetForPlay, validateRuleset, generateDetailsForRuleset }
